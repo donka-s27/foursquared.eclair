70c06bd34c0c8c8b5433f0c51b0301cadbcb80b6
Joe LaPenna <joe@joelapenna.com>
2009-12-25 00:14:40 -0600

    merge

---
+++ main/src/com/joelapenna/foursquared/preferences/CredentialsStore.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2009 Joe LaPenna
+ */
+package com.joelapenna.foursquared.preferences;
+
+/**
+ * @author Joe LaPenna (joe@joelapenna.com)
+ *
+ */
+public class CredentialsStore {
+
+}
